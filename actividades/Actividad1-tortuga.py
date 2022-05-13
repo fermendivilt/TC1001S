06bf92536932e4589c826b80679c5de43d43cd99
Fernando Mendivil Terminel <fermendivilt@gmail.com>
2022-05-12 17:08:09 -0700

    Deleted wildcard for turtle module

--- actividades/Actividad1-tortuga.py
+++ actividades/Actividad1-tortuga.py
@@ -11,71 +11,71 @@ Exercises
 
 from math import hypot
 
-from turtle import *
+import turtle
 
 from freegames import vector
 
 
 def line(start, end):
     """Draw line from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    goto(end.x, end.y)
+    turtle.up()
+    turtle.goto(start.x, start.y)
+    turtle.down()
+    turtle.goto(end.x, end.y)
 
 
 def square(start, end):
     """Draw square from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    turtle.up()
+    turtle.goto(start.x, start.y)
+    turtle.down()
+    turtle.begin_fill()
 
     for count in range(4):
-        forward(end.x - start.x)
-        left(90)
+        turtle.forward(end.x - start.x)
+        turtle.left(90)
 
-    end_fill()
+    turtle.end_fill()
 
 
 def newcircle(start, end):
     """Draw circle from start to end."""
-    up()
-    goto(start.x, (start.y + (start.y / 2)))
+    turtle.up()
+    turtle.goto(start.x, (start.y + (start.y / 2)))
     radius = round(hypot((start.x - end.x),(start.y - end.y)), 1)
-    begin_fill()
-    circle(radius, 360)
-    end_fill()
+    turtle.begin_fill()
+    turtle.circle(radius, 360)
+    turtle.end_fill()
 
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    turtle.up()
+    turtle.goto(start.x, start.y)
+    turtle.down()
+    turtle.begin_fill()
 
     for count in range(2):
-        forward(end.x - start.x)
-        left(90)
-        forward(end.y - start.y)
-        left(90)
+        turtle.forward(end.x - start.x)
+        turtle.left(90)
+        turtle.forward(end.y - start.y)
+        turtle.left(90)
 
-    end_fill()
+    turtle.end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    turtle.up()
+    turtle.goto(start.x, start.y)
+    turtle.down()
+    turtle.begin_fill()
 
     for count in range(3):
-        forward(end.x - start.x)
-        left(120)
+        turtle.forward(end.x - start.x)
+        turtle.left(120)
 
-    end_fill()
+    turtle.end_fill()
 
 
 def tap(x, y):
@@ -96,22 +96,22 @@ def store(key, value):
     state[key] = value
     
 def color(value):
-    pencolor(value)
-    fillcolor(value)
+    turtle.pencolor(value)
+    turtle.fillcolor(value)
 
 state = {'start': None, 'shape': line}
-setup(420, 420, 370, 0)
-onscreenclick(tap)
-listen()
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'k')
-onkey(lambda: color('white'), 'w')
-onkey(lambda: color('green'), 'g')
-onkey(lambda: color('blue'), 'b')
-onkey(lambda: color('red'), 'R')
-onkey(lambda: store('shape', line), 'l')
-onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', newcircle), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
-done()
+turtle.setup(420, 420, 370, 0)
+turtle.onscreenclick(tap)
+turtle.listen()
+turtle.onkey(turtle.undo, 'u')
+turtle.onkey(lambda: color('black'), 'k')
+turtle.onkey(lambda: color('white'), 'w')
+turtle.onkey(lambda: color('green'), 'g')
+turtle.onkey(lambda: color('blue'), 'b')
+turtle.onkey(lambda: color('red'), 'R')
+turtle.onkey(lambda: store('shape', line), 'l')
+turtle.onkey(lambda: store('shape', square), 's')
+turtle.onkey(lambda: store('shape', newcircle), 'c')
+turtle.onkey(lambda: store('shape', rectangle), 'r')
+turtle.onkey(lambda: store('shape', triangle), 't')
+turtle.done()
